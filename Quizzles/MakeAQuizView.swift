39025c2c534545bbd572151582568cf7db82337f
Amanda Björneskog <mandolina_95@hotmail.com>
2016-12-08 11:59:21 +0100

    include MakeaQuiz file

---
+++ Quizzles/MakeAQuizView.swift
@@ -0,0 +1,26 @@
+//
+//  DataViewController.swift
+//  Quizzles
+//
+//  Created by Filip Hintze on 2016-12-06.
+//  Copyright © 2016 Filip Hintze. All rights reserved.
+//
+
+import UIKit
+
+class MakeAQuizView: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+}
+

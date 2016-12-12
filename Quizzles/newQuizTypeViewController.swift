29297546bfbcc9e75bec9ecc7362b2ff89c5409f
Hintze <filip.hintze@me.com>
2016-12-12 21:14:09 +0100

    fixade nya listview för att välja quiztype, liknande kommer läggas till för de andra två alternativen

---
+++ Quizzles/newQuizTypeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  newQuizTypeViewController.swift
+//  Quizzles
+//
+//  Created by Filip Hintze on 2016-12-12.
+//  Copyright © 2016 Filip Hintze. All rights reserved.
+//
+
+import UIKit
+
+class newQuizTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

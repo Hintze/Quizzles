249b7a0901fa6cb239afda210b72e56a26cf96ce
Hintze <filip.hintze@me.com>
2016-12-28 19:07:46 +0100

    minor changes

--- Quizzles/MakeAQuizView.swift
+++ Quizzles/MakeAQuizView.swift
@@ -23,18 +23,51 @@ class MakeAQuizView: UIViewController {
     
 }
 
-class MakeAQuizView2: UIViewController{
+class MakeAQuizView2: UIViewController, UIPickerViewDelegate,  UIPickerViewDataSource {
+    
+    @IBOutlet weak var subjectButton: UIButton!
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.picker.dataSource = self
+        self.picker.delegate = self
+        self.picker.isHidden = true
+
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+        
+        
     }
     
+    @IBAction func subjectClicked(_ sender: Any) {
+        self.subjectButton.isHidden = true
+        self.picker.isHidden = false
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    
 }
 
 class MakeAQuizView3: UIViewController {
---
+++ Quizzles/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  Quizzles
+//
+//  Created by Filip Hintze on 2016-12-28.
+//  Copyright © 2016 Filip Hintze. All rights reserved.
+//
+
+import Foundation

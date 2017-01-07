fb7de2c4476453aade3c16fa2d9fa34a6048adae
Anton Hildingsson <hilding4@gmail.com>
2017-01-07 09:23:50 -0800

    added some fixes

--- Quizzles/SettingsView.swift
+++ Quizzles/SettingsView.swift
@@ -10,18 +10,18 @@ import UIKit
 
 class TeachersSettingsView: UIViewController {
     
-    @IBOutlet weak var emailLabel: UILabel!
-    
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var phoneLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailLabel: UILabel!
     var phoneNr="Phone Number: "
     var emailAdress="Email Adress: "
     var phone="0701234567"
     var email="teacher@mail.com"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.        
+        
         emailLabel.text=emailAdress + email
         phoneLabel.text=phoneNr + phone
     }
@@ -31,7 +31,7 @@ class TeachersSettingsView: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func changeText(_ sender: Any) {
+    @IBAction func changeTheText(_ sender: Any) {
         email=emailTextField.text!
         phone=phoneTextField.text!
         emailLabel.text=emailAdress + email
